dc40e928ab068a498d5ebdf9cf981a8e94f3bc02
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-12-29 22:12:35 +0500

    add 160

---
+++ LeetCode.Test/0101-0200/0160-IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.Test;
+
+public class _0160_IntersectionOfTwoLinkedLists_Test
+{
+    [Theory]
+    [InlineData(8, new[] { 4, 1, 8, 4, 5 }, new[] { 5, 6, 1, 8, 4, 5 }, 2, 3)]
+    [InlineData(2, new[] { 1, 9, 1, 2, 4 }, new[] { 3, 2, 4 }, 3, 1)]
+    [InlineData(0, new[] { 2, 6, 4 }, new[] { 1, 5 }, 3, 2)]
+    public void GetIntersectionNode(
+        int intersectVal,
+        int[] arrayA,
+        int[] arrayB,
+        int skipA,
+        int skipB)
+    {
+        // Arrange
+        ListNode headA = arrayA.ToListNode();
+        ListNode headB = arrayB.ToListNode();
+        SetupListNodes(headA, headB, skipA, skipB, intersectVal);
+
+        var sut = new _0160_IntersectionOfTwoLinkedLists();
+
+        // Act
+        var actual = sut.GetIntersectionNode(headA, headB);
+
+        // Assert
+        if (intersectVal == 0)
+        {
+            Assert.Null(actual);
+        }
+        else
+        {
+            while (skipB > 0)
+            {
+                headB = headB.next;
+                skipB--;
+            }
+            Assert.Equal(headB, actual);
+        }
+    }
+
+    private void SetupListNodes(
+        ListNode headA,
+        ListNode headB,
+        int skipA,
+        int skipB,
+        int intersectVal)
+    {
+        if (intersectVal == 0) return;
+
+        while (skipA-- > 1) { headA = headA.next; }
+        while (skipB-- > 1) { headB = headB.next; }
+        headA.next = headB.next;
+    }
+}
---
+++ LeetCode/0101-0200/0160-IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,44 @@
+﻿
+namespace LeetCode;
+
+public class _0160_IntersectionOfTwoLinkedLists
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        int a = GetLength(headA), b = GetLength(headB);
+        ListNode shortList, longList;
+        if (a > b)
+        {
+            longList = headA;
+            shortList = headB;
+            (a, b) = (b, a);
+        }
+        else
+        {
+            shortList = headA;
+            longList = headB;
+        }
+
+        while (b-- > a)
+            longList = longList.next;
+
+        // now lengths are equal
+        while (longList != shortList && longList is not null)
+        {
+            longList = longList.next;
+            shortList = shortList.next;
+        }
+        return longList == shortList ? longList : null;
+    }
+
+    private int GetLength(ListNode head)
+    {
+        int counter = 0;
+        while (head is not null)
+        {
+            head = head.next;
+            counter++;
+        }
+        return counter;
+    }
+}
