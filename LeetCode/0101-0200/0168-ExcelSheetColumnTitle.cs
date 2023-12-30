cf35586c8e890538bc21de5d56ad92dae220b7e1
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-12-30 21:39:14 +0500

    add problem 168

---
+++ LeetCode/0101-0200/0168-ExcelSheetColumnTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode;
+
+public class _0168_ExcelSheetColumnTitle
+{
+    private const int len = 26;
+    public string ConvertToTitle(int columnNumber)
+    {
+        var sb = new List<char>();
+        while (columnNumber-- > 0)
+        {
+            sb.Add(ConvertToChar(columnNumber));
+            columnNumber = columnNumber / len;
+        };
+        return string.Join(string.Empty, sb.Reverse<char>());
+    }
+
+    private char ConvertToChar(int num) => (char)(65 + (num % len));
+}
