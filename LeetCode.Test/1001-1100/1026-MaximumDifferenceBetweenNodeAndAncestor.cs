8fdcff66a865e91d2532bbb94f8362d7c5eec52f
Иксанов Семен Александрович <Iksanov.S.Al@sberbank.ru>
2024-01-11 11:44:17 +0500

    add 1026 solution

---
+++ LeetCode.Test/1001-1100/1026-MaximumDifferenceBetweenNodeAndAncestor.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Test;
+
+public class _1026_MaximumDifferenceBetweenNodeAndAncestor_Test
+{
+    [Theory]
+    [InlineData(new object[] { 8, 3, 10, 1, 6, null!, 14, null!, null!, 4, 7, null!, null!, 13 }, 7)]
+    [InlineData(new object[] { 1, null!, 2, null!, null!, null!, 0, null!, null!, null!, null!, null!, null!, null!, 3 }, 3)]
+    [InlineData(new object[] { 1, 1, 3, 4, 5, 6, 7 }, 6)]
+    public void MaxAncestorDiff(object[] nums, int expected)
+    {
+        var sut = new _1026_MaximumDifferenceBetweenNodeAndAncestor();
+        var actual = sut.MaxAncestorDiff(nums.ToTreeNode());
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ LeetCode/1001-1100/1026-MaximumDifferenceBetweenNodeAndAncestor.cs
@@ -0,0 +1,24 @@
+﻿
+namespace LeetCode;
+
+public class _1026_MaximumDifferenceBetweenNodeAndAncestor
+{
+    private int _max = 0;
+    public int MaxAncestorDiff(TreeNode root)
+    {
+        Traverse(root, root.val, root.val);
+        return _max;
+    }
+    private void Traverse(TreeNode root, int min, int max)
+    {
+        if (root is null)
+        {
+            _max = Math.Max(_max, Math.Abs(max - min));
+            return;
+        }
+        min = Math.Min(root.val, min);
+        max = Math.Max(root.val, max);
+        Traverse(root.left, min, max);
+        Traverse(root.right, min, max);
+    }
+}
