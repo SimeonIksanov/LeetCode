c40cc8f888909d285404ceb4671ca9fe7e3b5e06
Иксанов Семен Александрович <Iksanov.S.Al@sberbank.ru>
2024-01-12 18:08:14 +0500

    add solution 1704

---
+++ LeetCode.Test/1701-1800/1704-DetermineIfStringHalvesAreAlike.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Test;
+
+public class _1704_DetermineIfStringHalvesAreAlike_Test
+{
+    [Theory]
+    [InlineData("book", true)]
+    [InlineData("textbook", false)]
+    public void HalvesAreAlike(string s, bool expected)
+    {
+        var sut = new _1704_DetermineIfStringHalvesAreAlike();
+        bool actual = sut.HalvesAreAlike(s);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ LeetCode/1701-1800/1704-DetermineIfStringHalvesAreAlike.cs
@@ -0,0 +1,14 @@
+﻿
+namespace LeetCode;
+
+public class _1704_DetermineIfStringHalvesAreAlike
+{
+    private static readonly HashSet<char> _vowels = new HashSet<char>(new[] { 'a', 'e', 'i', 'o', 'u' });
+    public bool HalvesAreAlike(string s)
+    {
+        int mid = s.Length / 2;
+        return s[..mid].Count(isVowels) == s[mid..].Count(isVowels);
+    }
+
+    private bool isVowels(char v) => _vowels.Contains(char.ToLower(v));
+}
